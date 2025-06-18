7e66a0f805a278cba0f1762d994e504de468d6ac
jmarusak <jmarusak@users.noreply.github.com>
2025-06-18 13:16:12 -0400

    Remove htmx CDN link to index.html

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from appraiser!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI(
+    title="Appraiser API",
+    description="API to estimate the value of a item based on image and text",
+)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("index.html", "r") as f:
+        html_content = f.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
